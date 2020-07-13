d2b5c446b9fac36a536e67dde38e146737e4401c
merrillrudd <merrillrudd@gmail.com>
2020-07-13 11:56:35 -0700

    add q to output

--- R/do-extract-ref.R
+++ R/do-extract-ref.R
@@ -30,7 +30,8 @@ do_extract_ref <- function(dir = ".", model = "ref_lsd")
                           "SSB0now_r",
                           "MSY_r",
                           "Bmsy_r",
-                          "proj_F_jytrf"
+                          "proj_F_jytrf",
+                          "par_q_cpue_qy"
                           )
 
     # "proj_catch_commercial_jryt", 

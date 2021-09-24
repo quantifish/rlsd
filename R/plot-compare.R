ba6b887e56841aee24f4874694ce6bfd31bf1ce6
maitepons <mpons@uw.edu>
2021-09-23 17:32:45 -0700

    free y-axis

--- R/plot-compare.R
+++ R/plot-compare.R
@@ -277,7 +277,7 @@ plot_compare_ssb <- function(object_list,
       scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
       theme_lsd(base_size = 14) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      facet_wrap(~ Region) +
+      facet_wrap(~ Region, scales ="free_y") +
       geom_label(data = labs2, mapping = aes(x = Year, y = value, label = type), nudge_x = -5) +
       coord_cartesian(clip = "off")
 
@@ -346,7 +346,7 @@ plot_compare_ssb <- function(object_list,
       scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
       scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
       theme_lsd(base_size = 14) +
-      facet_wrap(~Region) +
+      facet_wrap(~Region, scales = "free_y") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       geom_label(data = labs2, mapping = aes(x = Year, y = value, label = type), nudge_x = -5) +
       coord_cartesian(clip = "off")
@@ -812,7 +812,7 @@ plot_compare_vb <- function(object_list, object_names, figure_dir = "compare_fig
       scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
       theme_lsd(base_size = 14) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      facet_wrap(~Region)
+      facet_wrap(~Region, scales = "free_y")
 
     if (nmod > 5) {
       q <- q +
@@ -884,7 +884,7 @@ plot_compare_vb <- function(object_list, object_names, figure_dir = "compare_fig
         scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
         theme_lsd(base_size = 14) +
         theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-        facet_wrap(~Region)
+        facet_wrap(~Region, scales = "free_y")
 
       if (nmod > 5) {
         q <- q +
@@ -945,7 +945,7 @@ plot_compare_vb <- function(object_list, object_names, figure_dir = "compare_fig
       # scale_colour_manual(values = cols_all, labels = object_names) +
       # guides(colour = guide_legend(override.aes = list(colour = cols_all, linetype = lty_all))) +
       # scale_linetype(guide=FALSE) +
-      scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
+      #scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
       xlab("Fishing year") + ylab("Relative adjusted vulnerable biomass (tonnes)") +
       scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
       scale_y_continuous(limits = c(0,NA), expand = expansion(mult = c(0, 0.1))) +
@@ -1006,13 +1006,13 @@ plot_compare_vb <- function(object_list, object_names, figure_dir = "compare_fig
         stat_summary(data = vb %>% filter(Year %in% years), fun = function(x) quantile(x, 0.5), geom = "point", size = 1.5, alpha = 0.75) +
         geom_hline(data = Bref %>% filter(Region == 1), aes(yintercept = value), lwd = 1.2) +
         geom_label(data = Bref %>% filter(Region == 1), label = "Reference", aes(x = min(vb$Year) + 10, y = value), size = 5, color = "black", fill = "white") +
-        scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
+        #scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
         xlab("Fishing year") + ylab("Adjusted vulnerable biomass (tonnes)") +
         scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
         scale_y_continuous(limits = c(0,NA), expand = expansion(mult = c(0, 0.1))) +
         theme_lsd(base_size = 14) +
         theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
-        facet_wrap(~Region)
+        facet_wrap(~Region, scales = "free_y")
 
       if (nmod > 5) {
         q <- q +
@@ -1069,7 +1069,7 @@ plot_compare_vb <- function(object_list, object_names, figure_dir = "compare_fig
       scale_x_continuous(breaks = seq(0, 1e6, 10), minor_breaks = seq(0, 1e6, 1)) +
       theme_lsd(base_size = 14) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      facet_wrap(~Region)
+      facet_wrap(~Region, scales ="free_y")
 
     if (nmod > 5) {
       q <- q +

4705253f716000df90d135b803e0b52c85cfc717
maitepons <mpons@uw.edu>
2021-10-07 11:07:25 -0700

    plots obs LFs by season and combined

--- R/plot-length-frequency.R
+++ R/plot-length-frequency.R
@@ -133,9 +133,8 @@ plot_lfs <- function(object,
     dlfw <- right_join(dlf, elf2, by = c("LF","Sex","Year","Source","Region","Season","lower","upper"))
 
     for (i in 1:data$n_area) {
-      for (j in unique(dlfw$Season)) {
-      p <- ggplot(data = filter(dlfw, Region %in% i, Season == j, Size >= lower & Size <= upper),
-                  aes(x = Size, y = fct_rev(paste(Year)), height = value, fill = Source, alpha = rawW)) +
+        p <- ggplot(data = filter(dlfw, Region %in% i, Size >= lower & Size <= upper),
+                  aes(x = Size, y = fct_rev(paste(Year, Season)), height = value, fill = Source, alpha = rawW)) +
         ggridges::geom_density_ridges(stat = "identity", scale = 3.5) +
         xlab(xlab) + ylab(ylab) +
         guides(shape = FALSE, colour = FALSE) +
@@ -148,11 +147,33 @@ plot_lfs <- function(object,
         theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
         labs(alpha = "weight")
       p <- p + facet_grid(~ Sex, scales = "free") +
-        ggtitle(paste("Region", i, "Season", j, sep="  ")) +
-        theme(plot.title = element_text(hjust = 0.5))
-      ggsave(paste0(figure_dir, "lf_obs_Region", i, j, ".png"), p, height = 10, width = 9)
+            theme(plot.title = element_text(hjust = 0.5))
+      ggsave(paste0(figure_dir, "lf_obs_Region", i, ".png"), p, height = 10, width = 9)
+      }
+
+    ####
+    for (i in 1:data$n_area) {
+      for (j in unique(dlfw$Season)) {
+        p <- ggplot(data = filter(dlfw, Region %in% i, Season == j, Size >= lower & Size <= upper),
+                    aes(x = Size, y = fct_rev(paste(Year)), height = value, fill = Source, alpha = rawW)) +
+          ggridges::geom_density_ridges(stat = "identity", scale = 3.5) +
+          xlab(xlab) + ylab(ylab) +
+          guides(shape = FALSE, colour = FALSE) +
+
+          scale_colour_manual(values = rev(ggplotColours(n = length(sources)))) +
+          scale_x_continuous(minor_breaks = seq(0, 1e6, 2), limits = c(30, max(elf$upper)),
+                             expand = c(0, 0)) +
+          scale_y_discrete(expand = c(0, 0)) +
+          theme_lsd() +
+          theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
+          labs(alpha = "weight")
+        p <- p + facet_grid(~ Sex, scales = "free") +
+          ggtitle(paste("Region", i, "Season", j, sep="  ")) +
+          theme(plot.title = element_text(hjust = 0.5))
+        ggsave(paste0(figure_dir, "lf_obs_Region", i, j, ".png"), p, height = 10, width = 9)
       }
     }
+
     for (i in 1:length(sq)) {
         pq <- sq[i]:(sq[i] + n_panel - 1)
         p <- ggplot() +

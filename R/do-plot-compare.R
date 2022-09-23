fb0b38350ba65d90946ddd322ced4514906f1633
merrillrudd <merrillrudd@gmail.com>
2022-09-22 22:42:32 -0700

    comment out resid table for now

--- R/do-plot-compare.R
+++ R/do-plot-compare.R
@@ -13,7 +13,7 @@ do_plot_compare <- function(object_list, object_names,
   dir.create(figure_dir, showWarnings = FALSE)
 
   if (single_stock == TRUE) {
-    table_compare_residuals(object_list = object_list, object_names = object_names, figure_dir = figure_dir)
+    # table_compare_residuals(object_list = object_list, object_names = object_names, figure_dir = figure_dir)
     table_compare_parameters(object_list = object_list, object_names = object_names, figure_dir = figure_dir)
     plot_compare_vb(object_list = object_list, object_names = object_names, figure_dir = figure_dir)
     plot_compare_recruitment(object_list = object_list, object_names = object_names, figure_dir = figure_dir)
